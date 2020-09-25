c58a6fc3f1a60da905e9117a40e5d43b43e38791
SleepNoMore <nninkovic.py@gmail.com>
2020-09-25 11:07:16 +0200

    Removed virt

--- src/virt/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\python\python_and_django_full_stack_web_developer_bootcamp\my_django_stuff\blogpage_v2\virt\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
